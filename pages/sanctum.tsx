0d7b2501fc870448e59174076d7abf614d7c5a20
anyabot <52899201+anyabot@users.noreply.github.com>
2023-09-28 22:13:01 +0900

    Optimized Sanctum

--- pages/sanctum.tsx
+++ pages/sanctum.tsx
@@ -1,27 +1,46 @@
-import { useAppSelector, useAppDispatch } from '@/hooks';
-import { selectSanctum, selectSanctumStatus, fetchSanctumAsync } from '@/store/sanctumSlice';
-import { useEffect, useState } from 'react';
-import Link from 'next/link'
-import Head from 'next/head'
-import { Flex, Button, Center, Select, Circle, HStack, VStack, Text, Image, Divider, Box } from '@chakra-ui/react';
-import { ArrowLeftIcon, ArrowRightIcon } from '@chakra-ui/icons';
-import { Floor, Wave } from '@/interfaces/sanctum';
-import EnemyGrid from '@/components/enemyGrid';
-import styles from "@/styles/custom.module.css"
+import { useAppSelector, useAppDispatch } from "@/hooks";
+import {
+  selectSanctum,
+  selectSanctumStatus,
+  fetchSanctumAsync,
+  setArea,
+  setDiff,
+  setFloor,
+  selectActiveArea,
+  selectActiveDiff,
+  selectActiveFloor,
+  selectFloorData,
+} from "@/store/sanctumSlice";
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Head from "next/head";
+import {
+  Flex,
+  Button,
+  Center,
+  Select,
+  Circle,
+  HStack,
+  VStack,
+  Text,
+  Image,
+  Divider,
+  Box,
+} from "@chakra-ui/react";
+import { ArrowLeftIcon, ArrowRightIcon } from "@chakra-ui/icons";
+import { Floor, Wave } from "@/interfaces/sanctum";
+import EnemyGrid from "@/components/enemyGrid";
+import styles from "@/styles/custom.module.css";
 
 export default function Home() {
-
   const sanctum = useAppSelector(selectSanctum);
-  const sanctumStatus = useAppSelector(selectSanctumStatus)
-  const [active, setActive] = useState("EW01")
-  const [floor, setFloor] = useState(1)
-  const [diff, setDiff] = useState(0)
-  const [actualDiff, setActualDiff] = useState(0)
-  const [actualFloor, setActualFloor] = useState<Floor[] | null>(null)
-  const [wave, setWave] = useState(0)
-  const [possibleWaves, setPossibleWaves] = useState<Wave[] | null>(null)
-  const [wavenum, setWaveNum] = useState(0)
-  const [actualWave, setActualWave] = useState<Wave | null>(null)
+  const sanctumStatus = useAppSelector(selectSanctumStatus);
+  const activeFloor = useAppSelector(selectActiveFloor);
+  const activeDiff = useAppSelector(selectActiveDiff);
+  const activeArea = useAppSelector(selectActiveArea);
+  const floorData = useAppSelector(selectFloorData);
+  const [wave, setWave] = useState(0);
+  const [waveData, setWaveData] = useState<Wave | null>(null);
 
   const dispatch = useAppDispatch();
 
@@ -29,110 +48,186 @@ export default function Home() {
     dispatch(fetchSanctumAsync());
   }, [dispatch]);
   useEffect(() => {
-    sanctum[active] ? floor >= sanctum[active].length ? setFloor(sanctum[active].length - 1) : null : null
-    sanctum[active] ? sanctum[active][floor] ? setActualFloor(sanctum[active][floor]) : null : null
-  }, [sanctum, active, floor]);
-  useEffect(() => {
-    if (actualFloor) {
-      actualFloor[actualDiff]?.waves ? wave >= actualFloor[actualDiff].waves.length ? setWave(actualFloor[actualDiff].waves.length - 1) : null : null
-      actualFloor[actualDiff]?.waves[wave] ? setPossibleWaves(actualFloor[actualDiff].waves[wave]) : null
-    }
-  }, [sanctum, actualFloor, wave, actualDiff]);
-  useEffect(() => {
-    if (actualFloor) {
-      console.log(1111, diff, actualFloor.length)
-      if (diff >= actualFloor.length) {
-        setActualDiff(actualFloor.length - 1)
-        setDiff(actualFloor.length - 1)
-      }
-      else {
-        setActualDiff(diff)
+    if (floorData?.waves) {
+      if (wave >= floorData.waves.length) {
+        setWave(floorData.waves.length - 1);
+        setWaveData(floorData.waves[floorData.waves.length - 1][0]);
+      } else {
+        setWaveData(floorData.waves[wave][0])
       }
     }
-  }, [sanctum, actualFloor, diff]);
-  useEffect(() => {
-    possibleWaves ? wavenum >= possibleWaves.length ? setWaveNum(possibleWaves.length - 1) : null : null
-    possibleWaves? possibleWaves[wavenum] ? setActualWave(possibleWaves[wavenum]) : null : null
-  }, [sanctum, possibleWaves, wavenum]);
+  }, [sanctum, floorData, wave]);
 
   function decreaseWave() {
-    wave > 0 ? setWave(wave - 1) : null
+    wave > 0 ? setWave(wave - 1) : null;
   }
   function increaseWave() {
-    if (actualFloor)
-    {
-      wave < actualFloor[actualDiff]!.waves.length - 1 ? setWave(wave + 1) : null
+    if (floorData) {
+      wave < floorData!.waves.length - 1 ? setWave(wave + 1) : null;
     }
   }
 
-  if (sanctumStatus == "failed"){
-    return (<>
-      <Head>
-        <title>Sanctum of Alteration</title>
-      </Head>
-      <h1>Fetch Failed</h1>
-    </>)
-  }
-  if (Object.keys(sanctum).length === 0) {
-    return (<>
-      <Head>
-        <title>Sanctum of Alteration</title>
-      </Head>
-      <h1>Loading</h1>
-    </>)
-  }
-  else {
+  if (sanctumStatus == "failed") {
     return (
       <>
         <Head>
           <title>Sanctum of Alteration</title>
         </Head>
-      
+        <h1>Fetch Failed</h1>
+      </>
+    );
+  }
+  if (Object.keys(sanctum).length === 0) {
+    return (
+      <>
+        <Head>
+          <title>Sanctum of Alteration</title>
+        </Head>
+        <h1>Loading</h1>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Head>
+          <title>Sanctum of Alteration</title>
+        </Head>
+
         <VStack as={Center}>
-          <Text as="b" fontSize="6xl" textAlign="center">Sanctum of Alteration</Text>
-          <Flex flexWrap={"wrap"} justifyContent="center" margin="auto" gap={{base: '1', md: '2'}}>
-            {Object.keys(sanctum).map((e, index) => (<Button size="md" key={e} isActive={active == e} colorScheme="red" onClick={() => setActive(e)}>Sanctum {index + 1}</Button>))}
+          <Text as="b" fontSize="6xl" textAlign="center">
+            Sanctum of Alteration
+          </Text>
+          <Flex
+            flexWrap={"wrap"}
+            justifyContent="center"
+            margin="auto"
+            gap={{ base: "1", md: "2" }}
+          >
+            {Object.keys(sanctum).map((e, index) => (
+              <Button
+                size="md"
+                key={e}
+                isActive={activeArea == e}
+                colorScheme="red"
+                onClick={() => dispatch(setArea(e))}
+              >
+                Sanctum {index + 1}
+              </Button>
+            ))}
           </Flex>
-          <Flex flexWrap={"wrap"} width="100%" justifyContent="center" gap={{base: '1', md: '2'}} flexDirection={{base: 'column', md: 'row'}}>
+          <Flex
+            flexWrap={"wrap"}
+            width="100%"
+            justifyContent="center"
+            gap={{ base: "1", md: "2" }}
+            flexDirection={{ base: "column", md: "row" }}
+          >
             <Box flex={1}>
-              <Select value={floor} onChange={(e) => setFloor(parseInt(e.target.value))} width="100%">
-                {sanctum[active].map((e, index) => {return e ? (<option key={index} value={index}>Floor {index}</option>) : null})}
+              <Select
+                value={activeFloor}
+                onChange={(e) => dispatch(setFloor(parseInt(e.target.value)))}
+                width="100%"
+              >
+                {sanctum[activeArea].map((e, index) => {
+                  return e ? (
+                    <option key={index} value={index}>
+                      Floor {index}
+                    </option>
+                  ) : null;
+                })}
               </Select>
             </Box>
-            <Flex flex={1} flexWrap={"wrap"} justifyContent="space-evenly" margin="auto">
-              <Button size="md" isActive={actualDiff == 0} colorScheme="blue" onClick={() => setDiff(0)}>EASY</Button>
-              {actualFloor && actualFloor?.length >= 2 ? <Button size="md" isActive={actualDiff == 1} disabled={!actualFloor || actualFloor?.length < 2} colorScheme="blue" onClick={() => setDiff(1)}>NORMAL</Button> : null}
-              {actualFloor && actualFloor?.length >= 3 ? <Button size="md" isActive={actualDiff == 2} disabled={!actualFloor || actualFloor?.length < 3} colorScheme="blue" onClick={() => setDiff(2)}>EXTREME</Button> : null}
+            <Flex
+              flex={1}
+              flexWrap={"wrap"}
+              justifyContent="space-evenly"
+              margin="auto"
+            >
+              <Button
+                size="md"
+                isActive={activeDiff == 0}
+                colorScheme="blue"
+                onClick={() => dispatch(setDiff(0))}
+              >
+                EASY
+              </Button>
+              {sanctum[activeArea][activeFloor]?.length >= 2 ? (
+                <Button
+                  size="md"
+                  isActive={activeDiff == 1}
+                  disabled={sanctum[activeArea][activeFloor]?.length < 2}
+                  colorScheme="blue"
+                  onClick={() => dispatch(setDiff(1))}
+                >
+                  NORMAL
+                </Button>
+              ) : null}
+              {sanctum[activeArea][activeFloor]?.length >= 3 ? (
+                <Button
+                  size="md"
+                  isActive={activeDiff == 2}
+                  disabled={sanctum[activeArea][activeFloor]?.length < 3}
+                  colorScheme="blue"
+                  onClick={() => dispatch(setDiff(2))}
+                >
+                  EXTREME
+                </Button>
+              ) : null}
             </Flex>
           </Flex>
-          
-          <Divider/>
 
-          {actualFloor && actualFloor[actualDiff] ? <HStack >{actualFloor[actualDiff].waves.map((e, index) => 
-            <div onClick={() => setWave(index)} key={index} className={styles["wave-button"]}>
-              {index == wave ? (<Image
-                src="/images/map-current.png"
-                alt="current-wave"
-                className={styles["wave-current"]}
-              />) : null}
-              <Image src="/images/profile/NightChick.png" alt={`wave-${index}`} />
-            </div>)}</HStack> : null}
-          {possibleWaves ? (<VStack><Select value={wavenum} onChange={(e) => setWaveNum(parseInt(e.target.value))}>
-            {possibleWaves.map((e, index) => {return e ? (<option key={index} value={index}>Formation {index + 1}</option>) : null})}
-          </Select>
-          <Text as="b">Formations for this Wave: {possibleWaves.length}</Text></VStack>) : null}
-          {actualWave ? 
+          <Divider />
+
+          {floorData ? (
+            <HStack>
+              {floorData.waves.map((e, index) => (
+                <div
+                  onClick={() => setWave(index)}
+                  key={index}
+                  className={styles["wave-button"]}
+                >
+                  {index == wave ? (
+                    <Image
+                      src="/images/map-current.png"
+                      alt="current-wave"
+                      className={styles["wave-current"]}
+                    />
+                  ) : null}
+                  <Image
+                    src="/images/profile/NightChick.png"
+                    alt={`wave-${index}`}
+                  />
+                </div>
+              ))}
+            </HStack>
+          ) : null}
+          {waveData ? (
             <HStack as={Center}>
-            <Circle as={Button} size='40px' bg='red' color='white' isDisabled={wave == 0} onClick={decreaseWave}>
-              <ArrowLeftIcon />
-            </Circle>
-            <EnemyGrid wave={actualWave.e}/>
-            <Circle as={Button} size='40px' bg='red' color='white' isDisabled={wave == (actualFloor && actualFloor[actualDiff] ? actualFloor[actualDiff]!.waves.length - 1 : false)} onClick={increaseWave}>
-              <ArrowRightIcon />
-            </Circle>
-          </HStack > : null }
+              <Circle
+                as={Button}
+                size="40px"
+                bg="red"
+                color="white"
+                isDisabled={wave == 0}
+                onClick={decreaseWave}
+              >
+                <ArrowLeftIcon />
+              </Circle>
+              <EnemyGrid wave={waveData.e} />
+              <Circle
+                as={Button}
+                size="40px"
+                bg="red"
+                color="white"
+                isDisabled={wave == floorData!.waves.length - 1}
+                onClick={increaseWave}
+              >
+                <ArrowRightIcon />
+              </Circle>
+            </HStack>
+          ) : null}
         </VStack>
       </>
-    )
+    );
   }
 }
